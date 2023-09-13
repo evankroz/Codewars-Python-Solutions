09a21b7a4bff153f0bdca6e8d88caf10bd4a5875
evankroz <117702363+evankroz@users.noreply.github.com>
2023-09-13 08:54:11 -0400

    Roman Numerals Decoder

---
+++ 6kyu /Roman Numerals Decoder.py
@@ -0,0 +1,14 @@
+roman_letters = (
+        ('M', 1000), ('CM', 900), ('D', 500), ('CD', 400), ('C', 100),
+        ('XC', 90), ('L', 50), ('XL', 40), ('X', 10), ('IX', 9), ('V', 5),
+        ('IV', 4), ('I', 1)
+)
+
+def solution(roman):
+    result = 0
+    index = 0
+    for i, c in roman_letters:
+        while roman[index : index+len(i)] == i:
+            result += c
+            index += len(i)
+    return result
