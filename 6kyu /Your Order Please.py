6e95ba620f693f08c84a56c4f0a6930b70a38527
evankroz <117702363+evankroz@users.noreply.github.com>
2024-03-13 11:32:56 -0400

    .py

---
+++ 6kyu /Your Order Please.py
@@ -0,0 +1,9 @@
+def order(sentence):
+    words = []
+
+    for i in range(1,10):
+        for word in sentence.split():
+            if str(i) in word:
+                words.append(word)
+
+    return " ".join(words)
