b8342f3f9c0beffd1ab12693236540e97e73a500
evan <117702363+evankroz@users.noreply.github.com>
2024-04-09 11:49:16 -0400

    not done yet

---
+++ 7kyu/Longest vowel chain.py
@@ -0,0 +1,10 @@
+def solve(s):
+    if not s:
+        return 0
+    vowels = ["o", "a", "e", "i", "u"]
+    res = ""
+    
+#longest chain of vowels in a string in python
+
+#if the string is in the set values, then increment position by 1 and check, recursively?
+    
